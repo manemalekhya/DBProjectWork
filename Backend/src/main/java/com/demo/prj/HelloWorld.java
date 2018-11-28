8c056066fd1ca3e98b04646db05c06a1e0169f41
Alekhya Manem <alekhyamanem@Alekhyas-MacBook-Pro.local>
2018-11-28 16:57:22 -0500

    json response

--- Backend/src/main/java/com/demo/prj/HelloWorld.java
+++ Backend/src/main/java/com/demo/prj/HelloWorld.java
@@ -1,19 +1,24 @@
 package com.demo.prj;
+
 import java.sql.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.util.ajax.JSON;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
  
 public class HelloWorld extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        response.setContentType("text/html");
+    	response.setContentType("application/json; charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello Servlet</h1>");
-        response.getWriter().println("session=" + request.getSession(true).getId());
+        //response.getWriter().println("<h1>Hello Servlet</h1>");
+        //response.getWriter().println("session=" + request.getSession(true).getId());
         System.out.println("Headers set!");
     	try {
     		Class.forName("com.mysql.jdbc.Driver");  
@@ -21,22 +26,30 @@ public class HelloWorld extends HttpServlet
     		System.out.println("Connection established!");
     		Statement stmt=con.createStatement();  
     		ResultSet rs=stmt.executeQuery("select * from customer"); 
+    		JSONArray result = new JSONArray();
     		System.out.println("Success");  
     			while (rs.next()) {
+    				JSONObject entry = new JSONObject();
     				int customer_id=rs.getInt("customer_id");
+    				entry.put("id", customer_id);
     		        String first_name = rs.getString("first_name");
+    				entry.put("fname", first_name);
     		        String last_name = rs.getString("last_name");
+    		        entry.put("lname", last_name);
     		        String phone_number = rs.getString("phone_number");
+    		        entry.put("phone_number", phone_number);
     		        String username = rs.getString("username");
-    		        System.out.println(customer_id + "::");
-    		        System.out.println(first_name + "::");
-    		        System.out.println(last_name + "::");
-    		        System.out.println(phone_number + "::");
-    		        System.out.println(username + "::");
-    		        response.getWriter().println("<h1>"+" "+rs.getInt(1)+rs.getString(2)+rs.getString(3)+rs.getString(4)+rs.getString(5)+" "+"</h1>");
+    		        System.out.print(customer_id + "::");
+    		        System.out.print(" " +first_name + "::");
+    		        System.out.print(" " +last_name + "::");
+    		        System.out.print(" " + phone_number + "::");
+    		        System.out.println(" " + username + "::");
+    		        result.add(entry);
+    		        //response.getWriter().println(JSON.toString(object));
     		     }
-    		con.close();  
+    			response.getWriter().println(JSON.toString(result));
+    			con.close();  
             }catch(Exception e){ System.out.println(e);}  
-    q }
+    }
   
 }
