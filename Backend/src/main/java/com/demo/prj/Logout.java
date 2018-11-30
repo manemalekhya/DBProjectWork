3829f6841f5b6c12b83b1b4fe8960216ef0e0dee
Lakshmi Ravichandran <lar146@pitt.edu>
2018-11-30 17:15:05 -0500

    db version 2

---
+++ Backend/src/main/java/com/demo/prj/Logout.java
@@ -0,0 +1,14 @@
+package com.demo.prj;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet
+{
+//Can be handled in front end?
+}
