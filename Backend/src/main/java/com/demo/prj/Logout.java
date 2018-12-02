670905163c355c42059e370da42c7c1451bf998f
Lakshmi Ravichandran <lar146@pitt.edu>
2018-12-02 18:19:13 -0500

    analytics and logout

--- Backend/src/main/java/com/demo/prj/Logout.java
+++ Backend/src/main/java/com/demo/prj/Logout.java
@@ -8,7 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class Logout extends HttpServlet
+private static final long serialVersionUID = 1L;
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 {
-//Can be handled in front end?
+HttpSession session = request.getSession(false); //Fetch session object
+if(session!=null) //If session is not null
+{
+session.invalidate(); //removes all session attributes bound to the session
+request.setAttribute("errMessage", "You have logged out successfully");
+System.out.println("sql input parameters check!");
+RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Login.jsp");
+requestDispatcher.forward(request, response);
+System.out.println("Logged out");
+}
 }
