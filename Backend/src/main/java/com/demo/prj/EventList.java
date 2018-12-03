29997cbefb281c1255cc29d363a7b730f244bc61
Alekhya Manem <alekhyamanem@cl2-wifi-10-215-36-245.wireless.pitt.edu>
2018-12-03 15:57:57 -0500

    EventList fix

--- Backend/src/main/java/com/demo/prj/EventList.java
+++ Backend/src/main/java/com/demo/prj/EventList.java
@@ -20,13 +20,15 @@ public class EventList extends HttpServlet
         response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         response.setStatus(HttpServletResponse.SC_OK);
         PrintWriter out = response.getWriter();
+        Connection con = null;
+        
         try 
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/book_my_game","root","mysql");   
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/book_my_game","root","mysql");   
             System.out.println("DB connection successful");
             
-            String eventSQLQuery="SELECT e.event_id as id,e.name as name, t1.team_name as team1, t2.team_name as team2, s.field_name as stadiumname, e.event_date as event_time from event_list as e, field_details as s, team_details as t1, team_details as t2  WHERE e.event_location = s.field_id AND (e.team1_id = t1.team_id AND e.team2_id = t2.team_id)";
+            String eventSQLQuery="SELECT e.event_id as id, e.name as name, t1.team_name as team1, t2.team_name as team2, s.field_name as stadiumname, e.event_date as event_time from event_list as e, field_details as s, team_details as t1, team_details as t2  WHERE e.event_location = s.field_id AND (e.team1_id = t1.team_id AND e.team2_id = t2.team_id)";
             
             if(request.getParameter("q")!=null){
                 String q=request.getParameter("q");
@@ -34,6 +36,13 @@ public class EventList extends HttpServlet
             }
 
             if(request.getParameter("c")!=null){
+            	if(request.getParameter("c")==null) {
+            		System.out.println("is null");
+            	}
+            	if(request.getParameter("c")=="") {
+            		System.out.println("is empty");
+            	}
+            	System.out.println(request.getParameter("c"));
                 eventSQLQuery+=" AND s.City LIKE \'%"+request.getParameter("c")+"%\'";
             }
 
@@ -45,21 +54,13 @@ public class EventList extends HttpServlet
                 eventSQLQuery+=" AND t1.sport_played LIKE \'%"+request.getParameter("sp")+"%\'";
                 eventSQLQuery+=" AND t2.sport_played LIKE \'%"+request.getParameter("sp")+"%\'";
             }
-
-            // String stadiumSQLQuery="SELECT * from field_details  WHERE city LIKE \"%" + cityName + "%\"";
             System.out.println(eventSQLQuery);
-
             Statement stmt=con.createStatement();
             java.sql.ResultSet rs = stmt.executeQuery(eventSQLQuery);
-            // System.out.println("Success");
             JSONArray result = new JSONArray();
             
             while(rs.next()){
-                // System.out.println(rs.getS);
                 JSONObject event = new JSONObject();
-            //     JSONObject coordinates = new JSONObject();
-            //     coordinates.put("lat" , rs.getFloat("Latitude"));
-            //     coordinates.put("lng" , rs.getFloat("Longitude"));
                 
                 event.put("id",rs.getInt("id"));
                 event.put("name",rs.getString("name"));
@@ -67,14 +68,29 @@ public class EventList extends HttpServlet
                 event.put("team2",rs.getString("team2"));
                 event.put("stadiumname",rs.getString("stadiumname"));
                 event.put("event_time",rs.getString("event_time"));
-            //     stadium.put("name",rs.getString("field_name"));
-            //     stadium.put("coordinates",coordinates);
-            //     System.out.println(stadium+"\n\n");
                 result.add(event);
             }
-            System.out.println(result);
+            
             response.getWriter().println(JSON.toString(result));
-            con.close();  
-        }catch(Exception e){ System.out.println(e);}  
+              
+        }catch(Exception e){ 
+        	JSONObject errRes = new JSONObject();
+        	JSONObject err = new JSONObject();
+        	err.put("code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        	err.put("message", e.getMessage());
+        	errRes.put("errors", err);
+        	response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        	response.getWriter().println(JSON.toString(errRes));
+        	System.out.println(e);
+        }finally {
+        	if(con != null) {
+        		try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	}
+        }
   }
 }
